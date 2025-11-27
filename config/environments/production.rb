6f9b94c2a70067ae4e1585bbb973ed46e1716997
奥平千沙都 <okuhirachisato@okudairachisatonoMacBook-Air.local>
2025-11-27 10:55:37 +0900

    add:S3設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.require_master_key = true
   config.hosts << /.*\.onrender\.com/
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
