1d29bbad88989fbf869f6f9b8f161735be6afee1
奥平千沙都 <okuhirachisato@okudairachisatonoMacBook-Air.local>
2025-12-01 17:51:48 +0900

    fix:lintエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
-  after_create :create_profile
+  after_create :create_default_profile
 
   private
 
   def create_default_profile
-    create_profile!
+    build_profile unless profile
+    profile.save!
   end
 end
