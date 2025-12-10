85f619d285e43fb7afbc13ccd1fcaaaf87ccf7d4
奥平千沙都 <okuhirachisato@okudairachisatonoMacBook-Air.local>
2025-12-10 14:39:21 +0900

    fix:css修正3

--- app/javascript/swiper_init.js
+++ app/javascript/swiper_init.js
@@ -1,15 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (document.querySelector(".myCatchSwiper")) {
-      new Swiper(".myCatchSwiper", {
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-    }
+document.addEventListener("turbo:load", () => {
+  if (!window.Swiper) {
+    console.log("Swiper がまだ読み込まれていません");
+    return;
+  }
+
+  console.log("swiper_init loaded");
+
+  const thumbs = new Swiper(".myThumbsSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesProgress: true,
   });
+
+  new Swiper(".myCatchSwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: thumbs,
+    },
+  });
+});
