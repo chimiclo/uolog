84e0578803ab908fe8fb51a7b0eee6814701eae6
奥平千沙都 <okuhirachisato@okudairachisatonoMacBook-Air.local>
2025-12-12 17:52:23 +0900

    Swiper修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link application.css
 //= link show_map.js
 //= link map.js
+//= link swiper_init.js
--- app/assets/javascripts/swiper_init.js
+++ app/assets/javascripts/swiper_init.js
@@ -1,18 +1,24 @@
-document.addEventListener("turbo:load", () => {
-  const main = document.querySelector(".myCatchSwiper");
-  const thumbs = document.querySelector(".myThumbsSwiper");
+document.addEventListener("DOMContentLoaded", () => {
+  const mainEl = document.querySelector(".myCatchSwiper");
+  const thumbEl = document.querySelector(".myThumbsSwiper");
 
-  if (!main || !thumbs) return;
+  if (!mainEl || !thumbEl) return;
+
+  console.log("🌀 SWIPER INIT LOADED");
 
   const thumbsSwiper = new Swiper(".myThumbsSwiper", {
-    slidesPerView: 4,
     spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
   });
 
   new Swiper(".myCatchSwiper", {
+    spaceBetween: 10,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
