b0870590b7be3dbed38bbe71764577a11073d8e1
奥平千沙都 <okuhirachisato@okudairachisatonoMacBook-Air.local>
2025-12-16 11:39:32 +0900

    fix:Rubocop修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     config.load_defaults 8.1
-    config.i18n.available_locales = [:ja]
+    config.i18n.available_locales = [ :ja ]
     config.i18n.default_locale = :ja
     config.autoload_lib(ignore: %w[assets tasks])
   end
